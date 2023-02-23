5ad899dd167637d9c4ac52ed1131a7446a7c9d30
k0r0z1f101 <9841584@bdeb.qc.ca>
2023-02-23 07:18:20 -0500

    Update .gitignore, AssetTools.NET.sln, and 236 more files...

---
+++ AssetTools.NET/Extra/AssetHelper.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace AssetsTools.NET.Extra
+{
+    public static class AssetHelper
+    {
+        public static List<AssetHelperMonoScriptInfo> GetAssetsFileScriptInfos(AssetsManager am, AssetsFileInstance inst)
+        {
+            List<AssetHelperMonoScriptInfo> infos = new List<AssetHelperMonoScriptInfo>();
+            AssetsFileMetadata metadata = inst.file.Metadata;
+            foreach (AssetPPtr scriptPPtr in metadata.ScriptTypes)
+            {
+                AssetTypeValueField msBaseField = am.GetExtAsset(inst, scriptPPtr.FileId, scriptPPtr.PathId).baseField;
+                string assemblyName = msBaseField["m_AssemblyName"].AsString;
+                string nameSpace = msBaseField["m_Namespace"].AsString;
+                string className = msBaseField["m_ClassName"].AsString;
+                
+                AssetHelperMonoScriptInfo info = new AssetHelperMonoScriptInfo(assemblyName, nameSpace, className);
+                infos.Add(info);
+            }
+
+            return infos;
+        }
+
+        public static string GetAssetNameFast(AssetsFile file, ClassDatabaseFile cldb, AssetFileInfo info)
+        {
+            ClassDatabaseType type = cldb.FindAssetClassByID(info.TypeId);
+            AssetsFileReader reader = file.Reader;
+
+            if (file.Metadata.TypeTreeEnabled)
+            {
+                ushort scriptId = file.GetScriptIndex(info);
+
+                TypeTreeType ttType = file.Metadata.FindTypeTreeTypeByID(info.TypeId, scriptId);
+
+                string ttTypeName = ttType.Nodes[0].GetTypeString(ttType.StringBuffer);
+                if (ttType.Nodes.Count == 0) return cldb.GetString(type.Name); // fallback to cldb
+                if (ttType.Nodes.Count > 1 && ttType.Nodes[1].GetNameString(ttType.StringBuffer) == "m_Name")
+                {
+                    reader.Position = info.AbsoluteByteStart;
+                    return reader.ReadCountStringInt32();
+                }
+                // todo, use the typetree since we have it already, there could be extra fields
+                else if (ttTypeName == "GameObject")
+                {
+                    reader.Position = info.AbsoluteByteStart;
+                    int size = reader.ReadInt32();
+                    int componentSize = file.Header.Version > 0x10 ? 0x0c : 0x10;
+                    reader.Position += size * componentSize;
+                    reader.Position += 0x04;
+                    return reader.ReadCountStringInt32();
+                }
+                else if (ttTypeName == "MonoBehaviour")
+                {
+                    reader.Position = info.AbsoluteByteStart;
+                    reader.Position += 0x1c;
+                    string name = reader.ReadCountStringInt32();
+                    if (name != "")
+                    {
+                        return name;
+                    }
+                }
+                return ttTypeName;
+            }
+
+            string typeName = cldb.GetString(type.Name);
+            if (type.ReleaseRootNode.Children.Count == 0) return typeName;
+            if (type.ReleaseRootNode.Children.Count > 1 && cldb.GetString(type.ReleaseRootNode.Children[0].FieldName) == "m_Name")
+            {
+                reader.Position = info.AbsoluteByteStart;
+                return reader.ReadCountStringInt32();
+            }
+            else if (typeName == "GameObject")
+            {
+                reader.Position = info.AbsoluteByteStart;
+                int size = reader.ReadInt32();
+                int componentSize = file.Header.Version > 0x10 ? 0x0c : 0x10;
+                reader.Position += size * componentSize;
+                reader.Position += 0x04;
+                return reader.ReadCountStringInt32();
+            }
+            else if (typeName == "MonoBehaviour")
+            {
+                reader.Position = info.AbsoluteByteStart;
+                reader.Position += 0x1c;
+                string name = reader.ReadCountStringInt32();
+                if (name != "")
+                {
+                    return name;
+                }
+            }
+            return typeName;
+        }
+    }
+
+    public struct AssetHelperMonoScriptInfo
+    {
+        public string assemblyName;
+        public string nameSpace;
+        public string className;
+
+        public AssetHelperMonoScriptInfo(string assemblyName, string nameSpace, string className)
+        {
+            this.assemblyName = assemblyName;
+            this.nameSpace = nameSpace;
+            this.className = className;
+        }
+    }
+}
---
+++ KotHModLoader/Program.cs
@@ -0,0 +1,80 @@
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using System.IO;
+
+void Load()
+{
+    //Vanilla manager
+    var assetsManagerVanilla = new AssetsManager();
+    assetsManagerVanilla.LoadClassPackage("lz4.tpk");
+
+    var afileInst = assetsManagerVanilla.LoadAssetsFile("../KingOfTheHat_Data/resources.assets", true);
+    var afile = afileInst.file;
+
+    assetsManagerVanilla.LoadClassDatabaseFromPackage(afile.Metadata.UnityVersion);
+    //
+
+    //Modded manager
+    var assetsManagerModded = new AssetsManager();
+    assetsManagerModded.LoadClassPackage("lz4.tpk");
+
+    var afileInstModded = assetsManagerModded.LoadAssetsFile("resources.assets.KIARAJUMPOPENMOUTH", true);
+    var afileModded = afileInstModded.file;
+
+    assetsManagerModded.LoadClassDatabaseFromPackage(afileModded.Metadata.UnityVersion);
+    //
+
+    //Mods folder
+    DirectoryInfo d = new DirectoryInfo(@"..\Mods");
+
+    FileInfo[] Files = d.GetFiles("*");
+    string str = "";
+
+    AssetsManager[] assetsManagersModded = new AssetsManager[Files.Length];
+    AssetsFileInstance[] afilesInstModded = new AssetsFileInstance[Files.Length];
+    AssetsFile[] afilesModded = new AssetsFile[Files.Length];
+
+    for (int a = 0; a < Files.Length; a++)
+    {
+        FileInfo file = Files[a];
+        assetsManagersModded[a] = new AssetsManager();
+        assetsManagersModded[a].LoadClassPackage("lz4.tpk");
+        afilesInstModded[a] = assetsManagersModded[a].LoadAssetsFile(file.Name, true);
+        afilesModded[a] = afilesInstModded[a].file;
+        assetsManagersModded[a].LoadClassDatabaseFromPackage(afilesModded[a].Metadata.UnityVersion);
+        str = str + ", " + file.Name;
+    }
+    Console.WriteLine(str);
+
+    Console.WriteLine("vanilla " + afile.GetAssetsOfType(AssetClassID.Texture2D).Count);
+    Console.WriteLine("modded " + afileModded.GetAssetsOfType(AssetClassID.Texture2D).Count);
+
+    var replacers = new List<AssetsReplacer>();
+    int i = 0;
+    foreach (var goInfo in afile.GetAssetsOfType(AssetClassID.Texture2D))
+    {
+        var goBaseVanilla = assetsManagerVanilla.GetBaseField(afileInst, goInfo);
+        var goInfoModded = afileModded.GetAssetsOfType(AssetClassID.Texture2D)[i];
+        var goBaseModded = assetsManagerModded.GetBaseField(afileInstModded, goInfoModded);
+        var name = goBaseVanilla["m_Name"].AsString;
+
+        if (goBaseModded["image data"].Value.ToString() != goBaseVanilla["image data"].Value.ToString())
+        {
+            Console.WriteLine(goBaseVanilla["m_Name"].AsString);
+            goBaseVanilla["image data"].Value = goBaseModded["image data"].Value;
+        }
+
+        replacers.Add(new AssetsReplacerFromMemory(afile, goInfo, goBaseVanilla));
+
+        i++;
+    }
+
+    var writer = new AssetsFileWriter("resources.assets" + ".mod");
+    afile.Write(writer, 0, replacers);
+    writer.Close();
+
+    Console.WriteLine("Press a key to exit.");
+    Console.ReadKey();
+}
+
+Load();
